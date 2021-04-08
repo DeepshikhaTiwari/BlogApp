a73921dfda1ae5e5740e1aee989314b20befb35c
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-04-08 18:24:58 +0530

    Add blog, blogauthor, and blogcomment in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Blog, BlogAuthor, BlogComment
+
+admin.site.register(BlogComment)
+admin.site.register(BlogAuthor)
+
+
+class BlogCommentsInline(admin.TabularInline):
+    model = BlogComment
+    max_num = 0
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'post_date')
+    inlines = [BlogCommentsInline]

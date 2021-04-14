aa1ca62dfcb681bd6441c93e06f6e85d10e1f557
Deepshikha Tiwari <shikhablb1@gmail.com>
2021-04-14 18:37:59 +0530

    Add path for blog(create and update and delete)

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,13 @@ urlpatterns = [
     path('blogger/<int:pk>', views.BlogListbyAuthorView.as_view(), name='blogs-by-author'),
     path('blog/<int:pk>', views.BlogDetailView.as_view(), name='blog-detail'),
     path('bloggers/', views.BloggerListView.as_view(), name='bloggers'),
+
     path('blog/<int:pk>/comment/', views.BlogCommentCreate.as_view(), name='blog_comment'),
+    path('author/<int:pk>/update/', views.BlogCommentUpdate.as_view(), name='blog-update'),
+    path('author/<int:pk>/delete/', views.BlogCommentDelete.as_view(), name='blog-delete'),
+
+    path('blog/create/', views.BlogCreate.as_view(), name='blog-create'),
+    path('blog/<int:pk>/update/', views.BlogUpdate.as_view(), name='blog-update'),
+    path('blog/<int:pk>/delete/', views.BlogDelete.as_view(), name='blog-delete'),
 
 ]
